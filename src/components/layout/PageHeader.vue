25c3791fc2a74d9ecc44aa8336e3c3a05091af50
Simon.J <896073330@qq.com>
2026-03-27 18:21:17 +0800

    去掉 header 显示

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import SimlessLogoSvg from '../assets/SimlessLogoSvg.vue'
-import { useLiffSessionStore } from '@/stores/liffSession'
-
-const liffSessionStore = useLiffSessionStore()
-const displayLineId = computed(() => liffSessionStore.userId)
-const displayRefLineId = computed(() => liffSessionStore.refereeUserId)
 </script>
 
 <template>
   <header class="header">
     <SimlessLogoSvg class="logo" width="86" height="25" aria-label="SIMLESS" />
-    <div v-if="displayLineId || displayRefLineId" class="line-id-debug">
-      <div v-if="displayLineId">
-        LINE ID: {{ displayLineId }}
-      </div>
-      <div v-if="displayRefLineId">
-        REF LINE ID: {{ displayRefLineId }}
-      </div>
-    </div>
   </header>
 </template>
 
@@ -41,15 +27,5 @@ const displayRefLineId = computed(() => liffSessionStore.refereeUserId)
   width: 86px;
   height: 25px;
   object-fit: contain;
-}
-
-.line-id-debug {
-  width: 100%;
-  font-size: 10px;
-  line-height: 1.35;
-  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace;
-  color: #444;
-  word-break: break-all;
-  text-align: center;
 }
 </style>
